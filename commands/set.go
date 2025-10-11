8e96afbf3def011b9bd76fe5e22d178ecc86c4a7
Mischa Rodermond <m.rodermond@nfir.nl>
2025-10-11 17:31:18 +0200

    Added options for expiration for SET

--- commands/set.go
+++ commands/set.go
@@ -2,14 +2,49 @@ package commands
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
 
+	"github.com/codecrafters-io/redis-starter-go/redis"
 	"github.com/codecrafters-io/redis-starter-go/resp"
 )
 
 const SET SupportedCommand = "set"
 
 func HandleSET(cmd *resp.RESPValue) (string, error) {
-	err := redisInstance.Set(cmd.Array[1], cmd.Array[2])
+	opts := make([]redis.CommandSetOption, 0)
+
+	for i := 0; i < len(cmd.Array); i++ {
+		option := strings.ToLower(cmd.Array[i].String)
+
+		var value int
+		var err error
+
+		if option == "ex" || option == "px" {
+			if len(cmd.Array) < i+1 {
+				return "", fmt.Errorf("error: missing argument for '%s'", option)
+			}
+
+			value, err = strconv.Atoi(cmd.Array[i+1].String)
+			if err != nil {
+				return "", fmt.Errorf("error: invalid argument '%s'", cmd.Array[i+1].String)
+			}
+
+			var expiry time.Time
+			if option == "ex" {
+				expiry = time.Now().Add(time.Duration(value) * time.Second)
+			}
+
+			if option == "px" {
+				expiry = time.Now().Add(time.Duration(value) * time.Millisecond)
+			}
+
+			opts = append(opts, redis.WithExpiration(&expiry))
+		}
+	}
+
+	err := redisInstance.Set(cmd.Array[1], cmd.Array[2], opts...)
 	if err != nil {
 		return "", fmt.Errorf("error handling SET")
 	}
