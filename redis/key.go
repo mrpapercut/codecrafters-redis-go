834a1d60a2dee675d0279a33524805f82e839cff
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2025-10-19 12:35:55 +0200

    Combined files 'get.go' and 'set.go' into 'keys.go'

--- redis/key.go
+++ redis/key.go
@@ -1,14 +1,51 @@
 package redis
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/codecrafters-io/redis-starter-go/resp"
 )
 
+const KEY_GET internalOperation = "KEY_GET"
 const KEY_SET internalOperation = "KEY_SET"
 
-type CommandOption func(*commandContextOption)
+func (r *Redis) Get(key string) (*resp.RESPValue, error) {
+	responseChan := make(chan internalResponse)
+
+	r.requestChan <- internalRequest{
+		operation:    KEY_GET,
+		key:          key,
+		responseChan: responseChan,
+	}
+
+	response := <-responseChan
+
+	return response.value, response.err
+}
+
+func (r *Redis) handleGet(req internalRequest) {
+	if r.isExpired(req.key) {
+		req.responseChan <- internalResponse{err: fmt.Errorf("key not found")}
+		return
+	}
+
+	value, ok := r.storage[req.key]
+	if !ok {
+		req.responseChan <- internalResponse{err: fmt.Errorf("key not found")}
+		return
+	}
+
+	if !value.IsKey() {
+		req.responseChan <- internalResponse{err: fmt.Errorf("operation against a key holding the wrong kind of value")}
+		return
+	}
+
+	req.responseChan <- internalResponse{
+		value: value.Key,
+		err:   nil,
+	}
+}
 
 type commandContextOption struct {
 	expire *time.Time
