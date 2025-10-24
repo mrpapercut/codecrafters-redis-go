60a0c1a885a6c2f2ad503b5ad7c418748009109d
Mischa Rodermond <m.rodermond@nfir.nl>
2025-10-24 12:04:35 +0200

    Initialize JSON slog

--- commands/commands_test.go
+++ commands/commands_test.go
@@ -1,8 +1,15 @@
 package commands
 
 import (
+	"log/slog"
+	"os"
 	"testing"
 )
+
+func init() {
+	defaultLogger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(defaultLogger)
+}
 
 func TestMain(m *testing.M) {
 	m.Run()
